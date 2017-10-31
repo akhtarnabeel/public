c119f300c6a2ea77beab3125d441a22ab0fb2a36
akhtarnabeel <akhtarnabeel@users.noreply.github.com>
2017-10-30 20:17:36 -0400

    update

---
+++ NFV-GENI/RyuController/setup_nfv_ryu_controller.sh
@@ -0,0 +1,5 @@
+cd ~
+wget https://github.com/akhtarnabeel/public/raw/master/NFV-GENI/RyuController/nfv_controller.py
+wget https://github.com/akhtarnabeel/public/raw/master/NFV-GENI/RyuController/nfv.config
+mv nfv_controller.py /tmp/ryu/ryu/app/
+mv nfv.config /tmp/ryu/ryu/app/
