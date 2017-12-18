199189fa1e8d0656410e503f22de22eef54ff5d5
akhtarnabeel <akhtarnabeel@users.noreply.github.com>
2017-12-17 22:51:22 -0500

    Updated NFV

---
+++ NFV-GENI/4VNFSStats/Upload.sh
@@ -0,0 +1,16 @@
+
+#vnf1:
+scp VNF1* root@198.129.50.8:~
+
+#vnf2:
+scp VNF2* root@198.129.50.13:~
+
+#vnf3:
+scp VNF3* root@198.129.50.5:~
+
+#vnf4:
+scp VNF4* root@198.129.50.7:~
+
+#controller:
+scp Con* root@198.129.50.14:~
+
